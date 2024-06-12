a20a77ea4c876184e19f4cf55d70f15c45d29125
AntonioBruschi <antonio.bruschi@edu.unifil.br>
2024-06-12 13:56:57 -0300

    desafio1

--- src/br/com/antonio/minhasmusicas/main/Main.java
+++ src/br/com/antonio/minhasmusicas/main/Main.java
@@ -1,7 +1,40 @@
-package br.com.antonio.minhasmusicas;
+package br.com.antonio.minhasmusicas.main;
+
+import br.com.antonio.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.antonio.minhasmusicas.modelos.Musica;
+import br.com.antonio.minhasmusicas.modelos.Podcast;
 
 public class Main {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("A little bit better");
+        minhaMusica.setArtista("Caleb Hearn");
 
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("PodPah");
+        meuPodcast.setHost("Igão e Mítico");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+    }
 }
 
 
--- src/br/com/antonio/minhasmusicas/modelos/Audio.java
+++ src/br/com/antonio/minhasmusicas/modelos/Audio.java
@@ -1,8 +1,36 @@
+package br.com.antonio.minhasmusicas.modelos;
+
 public class Audio {
     private String titulo;
-    private int duracao;
     private int totalReproducoes;
-    private int curtidas;
-    private double classificacao;
-    
+    private int totalCurtidas;
+    private int classificacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public int getClassificacao() {
+        return classificacao;
+    }
+
+    public void curte() {
+        this.totalCurtidas++;
+    }
+
+    public void reproduz() {
+        this.totalReproducoes++;
+    }
 }
